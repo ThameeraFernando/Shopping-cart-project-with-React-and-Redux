0b851fd980656db41fb6c0ab4c0262b9ade05766
ThameeraFernando <sithnuwan111@gmail.com>
2022-08-10 14:27:25 +0530

    create basic setup

--- src/App.js
+++ src/App.js
@@ -6,12 +6,36 @@ import CartContainer from "./components/CartContainer";
 import cartItems from "./cart-items";
 // redux stuff
 
+//store -stores data,think of state.(in the store  we store our data.)
+//reducer - function that used to update store.
+
+import { createStore } from "redux";
+//initial store
+const initialStore = {
+  count: 10,
+};
+//reducer
+//reducer -function that used to update store
+//two arguments-state,action
+//state -old state/state before update
+//action-what happened/what update
+//return updated old value
+const reducer = (state, action) => {
+  console.log("Hello world");
+  console.log({ state, action });
+  return state;
+};
+//create a store
+const store = createStore(reducer, initialStore);
+//get the state from the store
+console.log(store.getState());
+
 function App() {
   // cart setup
 
   return (
     <main>
-      <Navbar />
+      <Navbar cart={store.getState()} />
       <CartContainer cart={cartItems} />
     </main>
   );
